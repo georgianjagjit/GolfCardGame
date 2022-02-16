6e2d0ba5130e9345afb323b19c1c6e35e9bee3cf
georgianjagjit <35040051+georgianjagjit@users.noreply.github.com>
2022-02-15 20:05:03 -0700

    cpp setup files skelton

---
+++ src/CardModel.cpp
@@ -0,0 +1,6 @@
+/*
+* Volunteer - Golf Card Game
+* Gurdeep Singh, Jagjit Singh, Maruf Tonmoy
+* File: CardModel.cpp
+* Date Created: 15 Feb 2022
+*/
---
+++ src/GameController.cpp
@@ -0,0 +1,6 @@
+/*
+* Volunteer - Golf Card Game
+* Gurdeep Singh, Jagjit Singh, Maruf Tonmoy
+* File: GameController.cpp
+* Date Created: 15 Feb 2022
+*/
